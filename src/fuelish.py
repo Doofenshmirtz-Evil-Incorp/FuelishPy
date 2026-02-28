e2a1aa254f5d331c32e3880d67cbf9d604299cf7
Asvin Jain <89799369+Asvin1@users.noreply.github.com>
2026-02-28 22:59:37 +0530

    Update fuelish.py

--- src/fuelish.py
+++ src/fuelish.py
@@ -7,7 +7,7 @@ import concurrent.futures
 ###################################
 headers = {
     'Host': 'www.ndtv.com',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:143.0) Gecko/20100101 Firefox/143.0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:143.0) Gecko/20100101 Firefox/147.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate, br, zstd',
